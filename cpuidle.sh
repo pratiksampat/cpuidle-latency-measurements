349364826ab78789a72336503f5e4526a4ee51b1
Pratik Sampat <psampat@linux.ibm.com>
2021-05-17 12:18:33 +0530

    Update driver script for better processing of info

--- cpuidle.sh
+++ cpuidle.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
+#
+# CPU-Idle latency selftest provides support to systematically extract,
+# analyse and present IPI and timer based wakeup latencies for each CPU
+# and each idle state available on the system by leveraging the
+# test-cpuidle_latency module
+#
+# Author: Pratik R. Sampat <psampat@linux.ibm.com>
 
 LOG=cpuidle.log
 MODULE=./test-cpuidle_latency.ko
@@ -7,12 +14,17 @@ MODULE=./test-cpuidle_latency.ko
 # Kselftest framework requirement - SKIP code is 4.
 ksft_skip=4
 
-SMT=$(lscpu | grep "Thread(s) per core" | awk '{print $4}')
+RUN_TIMER_TEST=0
+VERBOSE=0
+
+DISABLE=1
+ENABLE=0
 
 helpme()
 {
 	printf "Usage: $0 [-h] [-todg args]
 	[-h <help>]
+	[-i <run timer tests>]
 	[-m <location of the module>]
 	[-o <location of the output>]
 	[-v <verbose>]
@@ -22,11 +34,14 @@ helpme()
 
 parse_arguments()
 {
-	while getopts ht:m:o:vt: arg
+	while getopts ht:m:o:vt:it: arg
 	do
 		case $arg in
 			h) # --help
 				helpme
+				;;
+			i) # run timer tests
+				RUN_TIMER_TEST=1
 				;;
 			m) # --mod-file
 				MODULE=$OPTARG
@@ -35,7 +50,7 @@ parse_arguments()
 				LOG=$OPTARG
 				;;
 			v) # Verbose mode
-				SMT=1
+				VERBOSE=1
 				;;
 			\?)
 				helpme
@@ -81,20 +96,15 @@ compute_average()
 	avg=$((sum/size))
 }
 
-# Disable all stop states
-disable_idle()
+cpu_is_online()
 {
-	for ((cpu=0; cpu<NUM_CPUS; cpu++))
-	do
-		local cpu_status=$(cpu_is_online $cpu)
-		if [ $cpu_status == 0 ]; then
-			continue
-		fi
-		for ((state=0; state<NUM_STATES; state++))
-		do
-			echo 1 > /sys/devices/system/cpu/cpu$cpu/cpuidle/state$state/disable
-		done
-	done
+	cpu=$1
+	if [ ! -f "/sys/devices/system/cpu/cpu$cpu/online" ]; then
+		echo 1
+		return
+	fi
+	status=$(cat /sys/devices/system/cpu/cpu$cpu/online)
+	echo $status
 }
 
 # Perform operation on each CPU for the given state
@@ -115,24 +125,23 @@ op_state()
 cpuidle_enable_state()
 {
 	state=$1
-	op_state 0 $state
+	op_state $ENABLE $state
 }
 
 cpuidle_disable_state()
 {
 	state=$1
-	op_state 1 $state
+	op_state $DISABLE $state
 }
 
-cpu_is_online()
+# Enable/Disable all stop states for all CPUs
+# $1 - Operation: enable (0) / disable (1)
+op_cpuidle()
 {
-	cpu=$1
-	if [ ! -f "/sys/devices/system/cpu/cpu$cpu/online" ]; then
-		echo 0
-		return
-	fi
-	status=$(cat /sys/devices/system/cpu/cpu$cpu/online)
-	echo $status
+	for ((state=0; state<NUM_STATES; state++))
+	do
+		op_state $1 $state
+	done
 }
 
 # Extract latency in microseconds and convert to nanoseconds
@@ -142,6 +151,73 @@ extract_latency()
 	do
 		latency=$(($(cat /sys/devices/system/cpu/cpu0/cpuidle/state$state/latency) * 1000))
 		latency_arr+=($latency)
+	done
+}
+
+# Simple linear search in an array
+# $1 - Element to search for
+# $2 - Array
+element_in()
+{
+	local item="$1"
+	shift
+	for element in "$@";
+	do
+		if [ "$element" == "$item" ]; then
+			return 0
+		fi
+	done
+	return 1
+}
+
+# Parse and return a cpuset with ","(individual) and "-" (range) of CPUs
+# $1 - cpuset string
+parse_cpuset()
+{
+	echo $1 | awk '/-/{for (i=$1; i<=$2; i++)printf "%s%s",i,ORS;next} 1' RS=, FS=-
+}
+
+extract_core_information()
+{
+	declare -a thread_arr
+	for ((cpu=0; cpu<NUM_CPUS; cpu++))
+	do
+		local cpu_status=$(cpu_is_online $cpu)
+		if [ $cpu_status == 0 ]; then
+			continue
+		fi
+
+		siblings=$(cat /sys/devices/system/cpu/cpu$cpu/topology/thread_siblings_list)
+		sib_arr=()
+		for c in $(parse_cpuset $siblings)
+		do
+			sib_arr+=($c)
+		done
+
+		if [ $VERBOSE == 1 ]; then
+			core_arr+=($cpu)
+			for thread in "${sib_arr[@]}"
+			do
+				if [ $cpu == 0 ]; then
+					first_core_arr+=($thread)
+				fi
+			done
+			continue
+		fi
+
+		element_in "${sib_arr[0]}" "${thread_arr[@]}"
+		if [ $? == 0 ]; then
+			continue
+		fi
+		core_arr+=(${sib_arr[0]})
+
+		for thread in "${sib_arr[@]}"
+		do
+			thread_arr+=($thread)
+			if [ $cpu == 0 ]; then
+				first_core_arr+=($thread)
+			fi
+		done
 	done
 }
 
@@ -171,13 +247,15 @@ ipi_test_once()
 run_ipi_tests()
 {
 	extract_latency
-	disable_idle
+	# Disable idle states for CPUs
+	op_cpuidle $DISABLE
+
 	declare -a avg_arr
-	echo -e "--IPI Latency Test---" >> $LOG
+	echo -e "--IPI Latency Test---" | tee -a $LOG
 
 	echo -e "--Baseline IPI Latency measurement: CPU Busy--" >> $LOG
 	printf "%s %10s %12s\n" "SRC_CPU" "DEST_CPU" "IPI_Latency(ns)" >> $LOG
-	for ((cpu=0; cpu<NUM_CPUS; cpu+=SMT))
+	for cpu in "${core_arr[@]}"
 	do
 		local cpu_status=$(cpu_is_online $cpu)
 		if [ $cpu_status == 0 ]; then
@@ -185,10 +263,15 @@ run_ipi_tests()
 		fi
 		ipi_test_once "baseline" $cpu
 		printf "%-3s %10s %12s\n" $src_cpu $cpu $ipi_latency >> $LOG
+		# Skip computing latency average from the source CPU to avoid bias
+		element_in "$cpu" "${first_core_arr[@]}"
+		if [ $? == 0 ]; then
+			continue
+		fi
 		avg_arr+=($ipi_latency)
 	done
 	compute_average "${avg_arr[@]}"
-	echo -e "Baseline Average IPI latency(ns): $avg" >> $LOG
+	echo -e "Baseline Avg IPI latency(ns): $avg" | tee -a $LOG
 
 	for ((state=0; state<NUM_STATES; state++))
 	do
@@ -196,7 +279,7 @@ run_ipi_tests()
 		echo -e "---Enabling state: $state---" >> $LOG
 		cpuidle_enable_state $state
 		printf "%s %10s %12s\n" "SRC_CPU" "DEST_CPU" "IPI_Latency(ns)" >> $LOG
-		for ((cpu=0; cpu<NUM_CPUS; cpu+=SMT))
+		for cpu in "${core_arr[@]}"
 		do
 			local cpu_status=$(cpu_is_online $cpu)
 			if [ $cpu_status == 0 ]; then
@@ -206,17 +289,22 @@ run_ipi_tests()
 			sleep 1
 			ipi_test_once "test" $cpu
 			printf "%-3s %10s %12s\n" $src_cpu $cpu $ipi_latency >> $LOG
+			# Skip computing latency average from the source CPU to avoid bias
+			element_in "$cpu" "${first_core_arr[@]}"
+			if [ $? == 0 ]; then
+				continue
+			fi
 			avg_arr+=($ipi_latency)
 		done
 		compute_average "${avg_arr[@]}"
 		echo -e "Expected IPI latency(ns): ${latency_arr[$state]}" >> $LOG
-		echo -e "Observed Average IPI latency(ns): $avg" >> $LOG
+		echo -e "Observed Avg IPI latency(ns) - State $state: $avg" | tee -a $LOG
 		cpuidle_disable_state $state
 	done
 }
 
 # Extract the residency in microseconds and convert to nanoseconds.
-# Add 100 ns so that the timer stays for a little longer than the residency
+# Add 200 ns so that the timer stays for a little longer than the residency
 extract_residency()
 {
 	for ((state=0; state<NUM_STATES; state++))
@@ -254,13 +342,15 @@ timeout_test_once()
 run_timeout_tests()
 {
 	extract_residency
-	disable_idle
+	# Disable idle states for all CPUs
+	op_cpuidle $DISABLE
+
 	declare -a avg_arr
-	echo -e "\n--Timeout Latency Test--" >> $LOG
+	echo -e "\n--Timeout Latency Test--" | tee -a $LOG
 
 	echo -e "--Baseline Timeout Latency measurement: CPU Busy--" >> $LOG
 	printf "%s %10s %10s\n" "Wakeup_src" "Baseline_delay(ns)">> $LOG
-	for ((cpu=0; cpu<NUM_CPUS; cpu+=SMT))
+	for cpu in "${core_arr[@]}"
 	do
 		local cpu_status=$(cpu_is_online $cpu)
 		if [ $cpu_status == 0 ]; then
@@ -271,7 +361,7 @@ run_timeout_tests()
 		avg_arr+=($timeout_diff)
 	done
 	compute_average "${avg_arr[@]}"
-	echo -e "Baseline Average timeout diff(ns): $avg" >> $LOG
+	echo -e "Baseline Avg timeout diff(ns): $avg" | tee -a $LOG
 
 	for ((state=0; state<NUM_STATES; state++))
 	do
@@ -279,7 +369,7 @@ run_timeout_tests()
 		echo -e "---Enabling state: $state---" >> $LOG
 		cpuidle_enable_state $state
 		printf "%s %10s %10s\n" "Wakeup_src" "Baseline_delay(ns)" "Delay(ns)" >> $LOG
-		for ((cpu=0; cpu<NUM_CPUS; cpu+=SMT))
+		for cpu in "${core_arr[@]}"
 		do
 			local cpu_status=$(cpu_is_online $cpu)
 			if [ $cpu_status == 0 ]; then
@@ -291,15 +381,16 @@ run_timeout_tests()
 		done
 		compute_average "${avg_arr[@]}"
 		echo -e "Expected timeout(ns): ${residency_arr[$state]}" >> $LOG
-		echo -e "Observed Average timeout diff(ns): $avg" >> $LOG
+		echo -e "Observed Avg timeout diff(ns) - State $state: $avg" | tee -a $LOG
 		cpuidle_disable_state $state
 	done
 }
 
 declare -a residency_arr
 declare -a latency_arr
+declare -a core_arr
+declare -a first_core_arr
 
-# Parse arguments
 parse_arguments $@
 
 rm -f $LOG
@@ -307,15 +398,17 @@ touch $LOG
 NUM_CPUS=$(nproc --all)
 NUM_STATES=$(ls -1 /sys/devices/system/cpu/cpu0/cpuidle/ | wc -l)
 
-# Insert the module
+extract_core_information
+
 ins_mod $MODULE
 
-printf "Started IPI latency tests\n"
 run_ipi_tests
+if [ $RUN_TIMER_TEST == 1 ]; then
+	run_timeout_tests
+fi
 
-printf "Started Timer latency tests\n"
-run_timeout_tests
-
+# Enable all idle states for all CPUs
+op_cpuidle $ENABLE
 printf "Removing $MODULE module\n"
-printf "Output logged at: $LOG\n"
+printf "Full Output logged at: $LOG\n"
 rmmod $MODULE
